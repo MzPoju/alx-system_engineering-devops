ea83c401e2c6a6edfdce5732e4defad7b452d8b6
Opeyemi Adepoju <adepojuoopeye@gmail.com>
2023-08-07 15:27:22 +0000

    Rest API

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# Script that exports data to JSON file
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    '''Gives name of employee and completed tasks and exports as JSON file'''
+    if len(argv) is not 2:
+        print("Command takes 2 arguments")
+        exit
+    _id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(_id)
+    req = requests.get(url)
+    jreq = req.json()
+    name = jreq['username']
+    url = "https://jsonplaceholder.typicode.com/todos"
+    req = requests.get(url)
+    jreq = req.json()
+    tasks = []
+    id_dict = {}
+    tasks_dict = []
+    for i in jreq:
+        if i['userId'] == int(_id):
+            tasks.append(i)
+    for i in tasks:
+        tasks_dict.append({'task': i['title'],
+                           'completed': i['completed'],
+                           'username': name
+                           })
+    id_dict = {_id: tasks_dict}
+    with open('{}.json'.format(_id), mode='w') as json_file:
+        json.dump(id_dict, json_file)
---
+++ 0x15-api/README.md
@@ -0,0 +1,4 @@
+REST API
+Friends don't let friends program in shell script
+Microservices
+PEP8 Python style
